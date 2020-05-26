34534c7fbf205adfa57c04594eabd168ef9a4879
Владислав Овчинников <vladovchinnikov950@gmail.com>
2020-05-26 03:27:00 +0300

    Init commit

---
+++ src/main/java/ic7cc/ovchinnikov/compiler/Application.java
@@ -0,0 +1,15 @@
+package ic7cc.ovchinnikov.compiler;
+
+import java_cup.runtime.ComplexSymbolFactory;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class Application {
+
+    public static void main(String... args) throws IOException {
+
+    }
+
+}

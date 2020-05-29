174014a66d327d0d7e1fa8c9ebf8adaebefe0b86
Владислав Овчинников <vladovchinnikov950@gmail.com>
2020-05-29 19:12:41 +0300

    Добавлены операции в AST

---
+++ src/main/java/ic7cc/ovchinnikov/compiler/ast/Operation.java
@@ -0,0 +1,26 @@
+package ic7cc.ovchinnikov.compiler.ast;
+
+public enum Operation {
+    POW,
+    CONCAT,
+    LESS,
+    NOT,
+    AND,
+    OR,
+    BAND,
+    DIV,
+    BLEFT,
+    MOREEQ,
+    LESSEQ,
+    EQUAL,
+    MUL,
+    ADD,
+    MOD,
+    BOR,
+    SUB,
+    BRIGHT,
+    LENGTH,
+    MORE,
+    NOTEQ,
+    UNM
+}

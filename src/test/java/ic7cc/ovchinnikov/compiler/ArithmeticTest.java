0020e37e67cb259b65dcf18eea54f881d6394fc9
Владислав Овчинников <vladovchinnikov950@gmail.com>
2020-05-30 21:01:50 +0300

    Добавлены тесты

---
+++ src/test/java/ic7cc/ovchinnikov/compiler/ArithmeticTest.java
@@ -0,0 +1,33 @@
+package ic7cc.ovchinnikov.compiler;
+
+import ic7cc.ovchinnikov.compiler.ast.node.Block;
+import ic7cc.ovchinnikov.compiler.lexer.Lexer;
+import ic7cc.ovchinnikov.compiler.parser.Parser;
+import ic7cc.ovchinnikov.compiler.util.XmlSerializer;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Path;
+
+public class ArithmeticTest {
+
+    private XmlSerializer xmlSerializer;
+
+    @Before
+    public void init() {
+        xmlSerializer = new XmlSerializer();
+    }
+
+    @Test
+    public void arithSimpleTest() throws Exception {
+        File file = Path.of("lua/arith/arith_simple.lua").toFile();
+
+        Parser parser = new Parser(new Lexer(new FileReader(file)));
+        Block block = (Block) parser.parse().value;
+
+        xmlSerializer.save(block, Path.of("result/arithmetic/xml/arith_simple.xml").toFile());
+    }
+
+}
--- src/test/java/ic7cc/ovchinnikov/compiler/BinopTest.java
+++ src/test/java/ic7cc/ovchinnikov/compiler/BinopTest.java
@@ -1,15 +1,173 @@
 package ic7cc.ovchinnikov.compiler;
 
+import ic7cc.ovchinnikov.compiler.ast.node.Block;
+import ic7cc.ovchinnikov.compiler.lexer.Lexer;
+import ic7cc.ovchinnikov.compiler.parser.Parser;
+import ic7cc.ovchinnikov.compiler.util.XmlSerializer;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
-import java.io.IOException;
+import java.io.FileReader;
+import java.nio.file.Path;
 
 public class BinopTest {
 
-    @Test
-    public void binopTestAnd() throws IOException {
+    private XmlSerializer xmlSerializer;
 
+    @Before
+    public void init() {
+        xmlSerializer = new XmlSerializer();
     }
 
+    @Test
+    public void binopTestAnd() throws Exception {
+        File file = Path.of("lua/binop/binop_and.lua").toFile();
+
+        Parser parser = new Parser(new Lexer(new FileReader(file)));
+        Block block = (Block) parser.parse().value;
+
+        xmlSerializer.save(block, Path.of("result/binop/xml/binop_and.xml").toFile());
+    }
+
+    @Test
+    public void binopConcat() throws Exception {
+        File file = Path.of("lua/binop/binop_concat.lua").toFile();
+
+        Parser parser = new Parser(new Lexer(new FileReader(file)));
+        Block block = (Block) parser.parse().value;
+
+        xmlSerializer.save(block, Path.of("result/binop/xml/binop_concat.xml").toFile());
+    }
+
+    @Test
+    public void binopTestDiv() throws Exception {
+        File file = Path.of("lua/binop/binop_div.lua").toFile();
+
+        Parser parser = new Parser(new Lexer(new FileReader(file)));
+        Block block = (Block) parser.parse().value;
+
+        xmlSerializer.save(block, Path.of("result/binop/xml/binop_div.xml").toFile());
+    }
+
+    @Test
+    public void binopTestEqual() throws Exception {
+        File file = Path.of("lua/binop/binop_eq.lua").toFile();
+
+        Parser parser = new Parser(new Lexer(new FileReader(file)));
+        Block block = (Block) parser.parse().value;
+
+        xmlSerializer.save(block, Path.of("result/binop/xml/binop_equal.xml").toFile());
+    }
+
+    @Test
+    public void binopTestMoreEq() throws Exception {
+        File file = Path.of("lua/binop/binop_ge.lua").toFile();
+
+        Parser parser = new Parser(new Lexer(new FileReader(file)));
+        Block block = (Block) parser.parse().value;
+
+        xmlSerializer.save(block, Path.of("result/binop/xml/binop_more_equal.xml").toFile());
+    }
+
+    @Test
+    public void binopTestMore() throws Exception {
+        File file = Path.of("lua/binop/binop_gt.lua").toFile();
+
+        Parser parser = new Parser(new Lexer(new FileReader(file)));
+        Block block = (Block) parser.parse().value;
+
+        xmlSerializer.save(block, Path.of("result/binop/xml/binop_more.xml").toFile());
+    }
+
+    @Test
+    public void binopTestLess() throws Exception {
+        File file = Path.of("lua/binop/binop_lt.lua").toFile();
+
+        Parser parser = new Parser(new Lexer(new FileReader(file)));
+        Block block = (Block) parser.parse().value;
+
+        xmlSerializer.save(block, Path.of("result/binop/xml/binop_less.xml").toFile());
+    }
+
+
+    @Test
+    public void binopTestLessEqual() throws Exception {
+        File file = Path.of("lua/binop/binop_le.lua").toFile();
+
+        Parser parser = new Parser(new Lexer(new FileReader(file)));
+        Block block = (Block) parser.parse().value;
+
+        xmlSerializer.save(block, Path.of("result/binop/xml/binop_less_equal.xml").toFile());
+    }
+
+    @Test
+    public void binopTestMod() throws Exception {
+        File file = Path.of("lua/binop/binop_mod.lua").toFile();
+
+        Parser parser = new Parser(new Lexer(new FileReader(file)));
+        Block block = (Block) parser.parse().value;
+
+        xmlSerializer.save(block, Path.of("result/binop/xml/binop_mod.xml").toFile());
+    }
+
+    @Test
+    public void binopTestNotEqual() throws Exception {
+        File file = Path.of("lua/binop/binop_neq.lua").toFile();
+
+        Parser parser = new Parser(new Lexer(new FileReader(file)));
+        Block block = (Block) parser.parse().value;
+
+        xmlSerializer.save(block, Path.of("result/binop/xml/binop_not_equal.xml").toFile());
+    }
+
+    @Test
+    public void binopTestOr() throws Exception {
+        File file = Path.of("lua/binop/binop_or.lua").toFile();
+
+        Parser parser = new Parser(new Lexer(new FileReader(file)));
+        Block block = (Block) parser.parse().value;
+
+        xmlSerializer.save(block, Path.of("result/binop/xml/binop_or.xml").toFile());
+    }
+
+    @Test
+    public void binopTestAdd() throws Exception {
+        File file = Path.of("lua/binop/binop_plus.lua").toFile();
+
+        Parser parser = new Parser(new Lexer(new FileReader(file)));
+        Block block = (Block) parser.parse().value;
+
+        xmlSerializer.save(block, Path.of("result/binop/xml/binop_add.xml").toFile());
+    }
+
+    @Test
+    public void binopTestPow() throws Exception {
+        File file = Path.of("lua/binop/binop_pow.lua").toFile();
+
+        Parser parser = new Parser(new Lexer(new FileReader(file)));
+        Block block = (Block) parser.parse().value;
+
+        xmlSerializer.save(block, Path.of("result/binop/xml/binop_pow.xml").toFile());
+    }
+
+    @Test
+    public void binopTestSub() throws Exception {
+        File file = Path.of("lua/binop/binop_sub.lua").toFile();
+
+        Parser parser = new Parser(new Lexer(new FileReader(file)));
+        Block block = (Block) parser.parse().value;
+
+        xmlSerializer.save(block, Path.of("result/binop/xml/binop_sub.xml").toFile());
+    }
+
+    @Test
+    public void binopTestTimes() throws Exception {
+        File file = Path.of("lua/binop/binop_times.lua").toFile();
+
+        Parser parser = new Parser(new Lexer(new FileReader(file)));
+        Block block = (Block) parser.parse().value;
+
+        xmlSerializer.save(block, Path.of("result/binop/xml/binop_times.xml").toFile());
+    }
 }
